7551cdaf8d2f4bc2345e662595f1e670c0acdfcb
Alex Paredes <alexparedesr95@gmail.com>
2021-11-04 22:32:00 -0600

    FEAT: Se crean colores personalizados.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
         extend: {
             backgroundImage: theme => ({
                 'body-pattern': "url('../img/pattern.png')"
-            })
+            }),
+            colors: {
+                "azul-claro": "#37bcf9",
+                "azul-oscuro": "#0370b9"
+            }
         }
     }
 }
